d2a07b9ff8d6dad0a861a3a7d7914e5693602189
Beata Łochowska <beata.lochowska@gmail.com>
2019-12-09 20:17:58 +0100

    task 3 completed

--- app.js
+++ app.js
@@ -2,21 +2,21 @@
   document.addEventListener("DOMContentLoaded", function() {
     const burger = document.querySelector(".burger");
     const nav = document.querySelector(".page-navigation__items");
-    const navItems = document.querySelectorAll(".page-navigation__item");
-    const buyButtons = document.querySelectorAll(".button");
     const links = document.querySelectorAll(".page-navigation__title");
-    const fun = document.querySelectorAll(".fun");
+
+    const contactForm = document.querySelector(".form");
+
+    contactForm.addEventListener("submit", function(event) {
+      event.preventDefault();
+      const formData = new FormData(contactForm);
+
+      for (let pair of formData.entries()) {
+        console.log(`${pair[0]}: ${pair[1]}`);
+      }
+    });
 
     burger.addEventListener("click", () => {
       nav.classList.toggle("page-navigation__active");
-
-      // navItems.forEach((link, index) => {
-      //   if (link.style.animation) {
-      //     link.style.animation = "";
-      //   } else {
-      //     link.style.animation = `navFade 1s ease forwards ${index / 4 + 0.5}s`;
-      //   }
-      // });
 
       burger.classList.toggle("toggle");
     });
@@ -24,7 +24,6 @@
     const buttonHolders = document.querySelectorAll(".button__holder");
     buttonHolders.forEach(holder => {
       const childButton = holder.querySelector("button");
-      console.log(childButton);
 
       childButton.addEventListener("click", function() {
         holder.classList.add("button__off");
