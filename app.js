555c7adbe5ea03fe48bcc43ab9b74a26caf54265
Beata Łochowska <beata.lochowska@gmail.com>
2019-12-09 17:56:11 +0100

    burger works

--- app.js
+++ app.js
@@ -4,23 +4,27 @@
     const nav = document.querySelector(".page-navigation__items");
     const navItems = document.querySelectorAll(".page-navigation__item");
     const buyButtons = document.querySelectorAll(".button");
-
-    const invisibleBurger = event => {
-      event.target.style.display = "none";
-    };
+    const links = document.querySelectorAll(".page-navigation__title");
 
     burger.addEventListener("click", () => {
       nav.classList.toggle("page-navigation__active");
 
-      navItems.forEach((link, index) => {
-        if (link.style.animation) {
-          link.style.animation = "";
-        } else {
-          link.style.animation = `navFade 1s ease forwards ${index / 4 + 0.5}s`;
-        }
-      });
+      // navItems.forEach((link, index) => {
+      //   if (link.style.animation) {
+      //     link.style.animation = "";
+      //   } else {
+      //     link.style.animation = `navFade 1s ease forwards ${index / 4 + 0.5}s`;
+      //   }
+      // });
 
       burger.classList.toggle("toggle");
+    });
+
+    links.forEach(item => {
+      item.addEventListener("click", event => {
+        nav.classList.remove("page-navigation__active");
+        burger.classList.remove("toggle");
+      });
     });
 
     buyButtons.forEach(item => {
