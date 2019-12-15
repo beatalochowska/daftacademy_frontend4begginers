2b7107f8a5b716fc21f1ae2a3cc1f702ce4ff66b
Beata Łochowska <beata.lochowska@gmail.com>
2019-12-15 18:57:01 +0100

    task 4 completed

--- app.js
+++ app.js
@@ -1,40 +1,49 @@
 (function() {
-  document.addEventListener("DOMContentLoaded", function() {
-    const burger = document.querySelector(".burger");
-    const nav = document.querySelector(".page-navigation__items");
-    const links = document.querySelectorAll(".page-navigation__title");
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".page-navigation__items");
+  const links = document.querySelectorAll(".page-navigation__title");
 
-    const contactForm = document.querySelector(".form");
+  const contactForm = document.querySelector(".form");
 
-    contactForm.addEventListener("submit", function(event) {
-      event.preventDefault();
-      const formData = new FormData(contactForm);
+  contactForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+    const formData = new FormData(contactForm);
 
-      for (let pair of formData.entries()) {
-        console.log(`${pair[0]}: ${pair[1]}`);
-      }
+    for (let pair of formData.entries()) {
+      console.log(`${pair[0]}: ${pair[1]}`);
+    }
+  });
+
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("page-navigation__active");
+
+    burger.classList.toggle("toggle");
+  });
+
+  links.forEach(item => {
+    item.addEventListener("click", event => {
+      nav.classList.remove("page-navigation__active");
+      burger.classList.remove("toggle");
     });
+  });
 
-    burger.addEventListener("click", () => {
-      nav.classList.toggle("page-navigation__active");
+  const concerts = document.querySelector("#concerts");
+  concerts.addEventListener("click", e => {
+    if (e.target.classList.contains("button")) {
+      const buyTicketButton = e.target;
+      const haveFunText = document.createElement("p");
+      haveFunText.textContent = "Have fun!";
 
-      burger.classList.toggle("toggle");
-    });
+      const buttonContainer = buyTicketButton.parentElement;
+      buttonContainer.insertBefore(haveFunText, buyTicketButton);
+      buyTicketButton.remove();
+    }
+  });
 
-    const buttonHolders = document.querySelectorAll(".button__holder");
-    buttonHolders.forEach(holder => {
-      const childButton = holder.querySelector("button");
-
-      childButton.addEventListener("click", function() {
-        holder.classList.add("button__off");
-      });
-    });
-
-    links.forEach(item => {
-      item.addEventListener("click", event => {
-        nav.classList.remove("page-navigation__active");
-        burger.classList.remove("toggle");
-      });
+  let image = document.querySelectorAll(".gallery__item > .concerts__image");
+  image.forEach(item => {
+    item.addEventListener("mouseleave", event => {
+      event.target.style.filter = "sepia(1)";
     });
   });
 })();
